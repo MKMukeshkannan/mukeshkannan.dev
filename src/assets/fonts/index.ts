1aa8368cce74733c7f9a3b92992f59e294933099
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-03-10 00:16:30 +0530

    INIT COMMIT
    Changes:
    - Created nextjs project
    - Added required fonts
    - Created a simple hero section with big typefaces.

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,20 @@
+import { Inter, Public_Sans } from "next/font/google";
+import localFont from "next/font/local";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--inter-font",
+});
+
+export const publicSans = Public_Sans({
+  weight: ["800"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--publicSans-font",
+});
+
+export const carlgine = localFont({
+  src: "./CarlgineRegular.ttf",
+  variable: "--carlgine-font",
+});
